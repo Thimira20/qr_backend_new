a1b0eda9eaf6b046a1873ff1362d80578876c529
Thimita20 <thimirapanditha20@gmail.com>
2025-02-12 02:30:03 +0530

    server update 3

--- server.js
+++ server.js
@@ -15,11 +15,19 @@ app.use(express.json());
 app.use(
   cors({
     origin: ["http://localhost:3001", "https://qr-code-v2-brown.vercel.app/"],
-    methods: "GET,POST,PUT,DELETE",
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
     allowedHeaders: "Content-Type,Authorization",
     credentials: true,
   })
 );
+app.options("*", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.status(200).end();
+});
+
 
 const createAdminUser = async () => {
   try {
