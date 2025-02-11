5e59b223aca2658b2893d1febeb6f9ad1217814b
Thimita20 <thimirapanditha20@gmail.com>
2025-02-12 02:04:51 +0530

    update server

--- server.js
+++ server.js
@@ -10,8 +10,16 @@ const cors = require("cors");
 const app = express();
 const User = require("./models/userModel");
 const bcrypt = require("bcryptjs");
-app.use(cors());
+//app.use(cors());
 app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3001", "https://qr-code-v2-brown.vercel.app/"],
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+    credentials: true,
+  })
+);
 
 const createAdminUser = async () => {
   try {
